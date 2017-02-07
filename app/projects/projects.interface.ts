1ea42217432d0d792b08fe048b5af685f6ad650a
Arthur <a.hugo.maximiliano@avanade.com>
2017-02-07 15:21:08 -0200

    projects.interface.ts #1 projects.ts #1

---
+++ app/projects/projects.interface.ts
@@ -0,0 +1,9 @@
+export interface IProjects {
+    SchProjectId: number;
+    Name: string;
+    CustomerId: number;
+    DtInitial: Date;
+    DtFinal: Date;
+    WBS: string;
+    SponsorId: number;
+}
