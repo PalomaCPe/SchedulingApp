0625217f73f835dec7fa1759ac23de9b32bfd312
Arthur <a.hugo.maximiliano@avanade.com>
2017-02-07 16:33:20 -0200

    Implementando Componentes -PROJETOS

--- app/projects/projects.component.ts
+++ app/projects/projects.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Projects } from './projects';
 
 @Component({
   moduleId: module.id,
   selector: 'ava-pro-app',
   templateUrl: 'projects.html'
 })
-export class AppProjects  {
-    pageName = "Projeto";
-}
+
+export class ProjectComponent{
+  private model: Projects = {
+      schProjectId: 1,
+      name: "Projeto Teste",
+      customerId: 1,
+      dtInitial: new Date(),
+      dtFinal: new Date(),
+      wbs: "WBSTESTE",
+      sponsorId: 1,
+  } as Projects;
+    
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+}  
