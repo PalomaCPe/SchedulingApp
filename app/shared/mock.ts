83294e3bce06769cf0df7112c4f5c77acffcf7c9
Hebert Paziam <hebert.paziam@avanade.com>
2017-02-08 17:41:37 -0200

    mock updated

--- app/shared/mock.ts
+++ app/shared/mock.ts
@@ -29,21 +29,21 @@ export const CUSTOMERS: Customer[] = [
         "name": "customer 1",
         "contact": "11 5555-1111",
         "email": "customer1@avanade.com",
-        "sponsor": 123
+        "sponsor": 2
     },
     {
         "id": 2,
         "name": "customer 2",
         "contact": "11 5555-2222",
         "email": "customer2@avanade.com",
-        "sponsor": 321
+        "sponsor": 3
     },
     {
         "id": 3,
         "name": "customer 3",
         "contact": "11 5555-3333",
         "email": "custome31@avanade.com",
-        "sponsor": 213
+        "sponsor": 1
     }
 ];
 
