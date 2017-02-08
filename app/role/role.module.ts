5c8aa78912603e0594402ba09b068f80c0664003
mayaracardoso <mayara.lopes.cardoso@avanade.com>
2017-02-08 13:02:42 -0200

    Add form to role page

--- app/role/role.module.ts
+++ app/role/role.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SharedModule } from '../shared/shared.module';
 
 import { RoleComponent } from './role.component';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, SharedModule],
     declarations: [RoleComponent],
     bootstrap: [RoleComponent]
 })
