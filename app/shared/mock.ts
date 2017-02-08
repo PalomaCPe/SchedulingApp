a1f3dbce69b25ec8c65899bd3e89b8abb52e6a8b
Paloma <p.cristina.pereira@avanade.com>
2017-02-08 13:33:01 -0200

    Importação do shared module em bookings.

--- app/shared/mock.ts
+++ app/shared/mock.ts
@@ -9,7 +9,7 @@ export const BOOKINGS: Booking[] = [
         "id": 1,
         "startDate": new Date(Date.now()),
         "endDate": new Date(Date.now()),
-        "percentual": 0.3,
+        "percentual": 30,
         "projectId": 1,
         "professionalId": 1
     },
@@ -17,7 +17,7 @@ export const BOOKINGS: Booking[] = [
         "id": 2,
         "startDate": new Date(Date.now()),
         "endDate": new Date(Date.now()),
-        "percentual": 0.5,
+        "percentual": 50,
         "projectId": 1,
         "professionalId": 1
     }
