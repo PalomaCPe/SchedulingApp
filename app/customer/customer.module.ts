6775f5964f4a868f3b3e0021acb68629a655975c
Hebert Paziam <hebert.paziam@avanade.com>
2017-02-08 12:54:04 -0200

    customer template updated; header page name update

--- app/customer/customer.module.ts
+++ app/customer/customer.module.ts
@@ -1,12 +1,14 @@
-import { NgModule }         from'@angular/core';
-import { BrowserModule }    from'@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { SharedModule } from './../shared/shared.module';
 import { CustomerComponent } from './customer.component';
 
-@NgModule ({
-    imports:        [BrowserModule],
-    declarations:   [CustomerComponent],
-    bootstrap:      [CustomerComponent]
+@NgModule({
+    imports: [BrowserModule, FormsModule, SharedModule],
+    declarations: [CustomerComponent],
+    bootstrap: [CustomerComponent]
 })
 
 export class CustomerModule { }
