8613612ee6a8569fb0e8178775deb29eafc59082
Victor <v.munhoz.theodoro@avanade.com>
2017-02-09 12:29:59 -0200

    professional service feed done

--- app/professional/professional.module.ts
+++ app/professional/professional.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { ProfessionalComponent } from './professional.component';
 import { EmailHider } from '../shared/filter/EmailHider';
 import { SharedModule } from '../shared/shared.module'
+import { ProfessionalService } from './professional.service';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, SharedModule],
     declarations: [ProfessionalComponent, EmailHider],
+    providers: [ProfessionalService],
     bootstrap: [ProfessionalComponent]
 })
 export class ProfessionalModule { }
