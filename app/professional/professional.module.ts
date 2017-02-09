b94cad1c4227683d90e30cc03a005b8db9cf345c
Victor <v.munhoz.theodoro@avanade.com>
2017-02-09 17:48:34 -0200

    professional details updates

--- app/professional/professional.module.ts
+++ app/professional/professional.module.ts
@@ -4,13 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Router } from '@angular/router';
 
 import { ProfessionalComponent } from './professional.component';
+import { ProfessionalDetailComponent } from './professional-details.component'
+
 import { EmailHider } from '../shared/filter/EmailHider';
 import { SharedModule } from '../shared/shared.module'
 import { ProfessionalService } from './professional.service';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, SharedModule, RouterModule.forRoot([
-        {path:'professionals', component: ProfessionalComponent}
+        {path:'professionals', component: ProfessionalComponent},
+        {path:'professionals/detail/:id', component: ProfessionalDetailComponent}
     ])],
     declarations: [ProfessionalComponent, EmailHider],
     providers: [ProfessionalService]
