7a6d3547359e69cf2645aa9666890efb189e2eed
Arthur <a.hugo.maximiliano@avanade.com>
2017-02-09 10:20:22 -0200

    links para as chaves estr. do modulo PROJETO

--- app/project/project.component.ts
+++ app/project/project.component.ts
@@ -1,21 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Project } from './project';
 
+import { Project } from './project';
 import { PROJECTS } from '../shared/mock';
 
+import { Professional } from '../professional/professional';
+import { PROFESSIONALS } from '../shared/mock';
+
+import { Customer } from '../customer/customer';
+import { CUSTOMERS } from '../shared/mock';
 
 @Component({
   moduleId: module.id,
   selector: 'ava-pro-app',
-  templateUrl: 'projects.html'
+  templateUrl: 'project.html'
 })
 
 export class ProjectComponent{
   pageName = "Projetos";
+  
   projects: Project[] = PROJECTS;
+  professionals: Professional[];
+  customers: Customer[];
+
   selectedProject: Project;
+  
   projectDetails(project: Project): void {
     this.selectedProject = project;
+   
+    this.selectedProject.professional = PROFESSIONALS.find(p => p.professionalId === project.sponsorId);
+    this.selectedProject.customer = CUSTOMERS.find(p => p.id === project.customerId);
+    
+    this.professionals = PROFESSIONALS;
+    this.customers = CUSTOMERS;
   }
+  
 }  
 
--- app/project/project.ts
+++ app/project/project.ts
@@ -1,11 +1,17 @@
 import { IProject } from './project.interface';
+import { Professional } from '../professional/professional';
+import { Customer } from '../customer/customer';
 
 export class Project implements IProject {
     projectId: number;
     name: string;
-    customerId: number;
     dtInitial: Date;
     dtFinal: Date;
     wbs: string;
+
+    customerId: number;
     sponsorId: number;
+
+    customer: Customer;
+    professional: Professional;
 }
