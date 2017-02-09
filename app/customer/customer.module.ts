799166fbcb3c9ae2aceb809b437e7bfb4d19c250
Hebert Paziam <hebert.paziam@avanade.com>
2017-02-09 15:48:47 -0200

    customer included on app routes.

--- app/customer/customer.module.ts
+++ app/customer/customer.module.ts
@@ -1,3 +1,4 @@
+import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -8,10 +9,12 @@ import { CustomerComponent } from './customer.component';
 import { CustomerService } from './customer.service';
 
 @NgModule({
-    imports: [BrowserModule, FormsModule, SharedModule],
+    imports: [BrowserModule, FormsModule, SharedModule,
+        RouterModule.forRoot([
+            { path: 'customers', component: CustomerComponent },
+        ])],
     declarations: [CustomerComponent],
-    providers: [CustomerService],
-    bootstrap: [CustomerComponent]
+    providers: [CustomerService]
 })
 
 export class CustomerModule { }
