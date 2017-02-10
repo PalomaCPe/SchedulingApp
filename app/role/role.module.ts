27fdada3292df378c1c7f89ddfa919f465062a0a
mayaracardoso <mayara.lopes.cardoso@avanade.com>
2017-02-10 09:43:00 -0200

    Role details

--- app/role/role.module.ts
+++ app/role/role.module.ts
@@ -5,13 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { RoleService } from './role.service';
 import { RoleComponent } from './role.component';
+import { RoleDetailsComponent } from './role-details.component';
 
 @NgModule({
     imports: [BrowserModule, FormsModule, SharedModule, RouterModule.forRoot([
-        { path: 'roles', component: RoleComponent}
+        { path: 'roles', component: RoleComponent},
+        { path: 'roles/details/:id', component: RoleDetailsComponent}
     ])],
 
-    declarations: [RoleComponent],
+    declarations: [RoleComponent, RoleDetailsComponent],
     providers: [RoleService]
 })
 
