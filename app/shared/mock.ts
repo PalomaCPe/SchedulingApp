b017b377b9cbe83f0c3f371fa33d4359af7eb1b6
thiagocolen <thiago.souzacolen@gmail.com>
2017-02-10 15:24:00 -0200

    fix CUSTOMERS and ROLES constant

--- app/shared/mock.ts
+++ app/shared/mock.ts
@@ -29,25 +29,25 @@ export const BOOKINGS: Booking[] = [
 
 export const CUSTOMERS: Customer[] = [
     {
-        "id": 1,
-        "name": "customer 1",
-        "contact": "11 5555-1111",
-        "email": "customer1@avanade.com",
-        "sponsor": 2
+        id: 1,
+        name: "customer 1",
+        contact: "11 5555-1111",
+        email: "customer1@avanade.com",
+        sponsor: 2
     },
     {
-        "id": 2,
-        "name": "customer 2",
-        "contact": "11 5555-2222",
-        "email": "customer2@avanade.com",
-        "sponsor": 3
+        id: 2,
+        name: "customer 2",
+        contact: "11 5555-2222",
+        email: "customer2@avanade.com",
+        sponsor: 3
     },
     {
-        "id": 3,
-        "name": "customer 3",
-        "contact": "11 5555-3333",
-        "email": "custome31@avanade.com",
-        "sponsor": 1
+        id: 3,
+        name: "customer 3",
+        contact: "11 5555-3333",
+        email: "custome31@avanade.com",
+        sponsor: 1
     }
 ];
 
@@ -132,25 +132,24 @@ export const PROJECTS: Project[] = [
 
 export const ROLES: Role[] = [
     {
-        "id": 120,
-        "name": "Associate Software Engineer",
-        "brc": "A0022",
-        "description": "Cargo mais baixo da Avanade",
-        "level": 10
+        id: 120,
+        name: "Associate Software Engineer",
+        brc: "A0022",
+        description: "Cargo mais baixo da Avanade",
+        level: 12
     },
     {
-        "id": 121,
-        "name": "Associate Software Engineer",
-        "brc": "A0022",
-        "description": "Cargo mais baixo da Avanade",
-        "level": 10
+        id: 121,
+        name: "Associate Software Engineer",
+        brc: "A0022",
+        description: "Cargo mais baixo da Avanade",
+        level: 9
     },
     {
-        "id": 122,
-        "name": "Associate Software Engineer",
-        "brc": "A0022",
-        "description": "Cargo mais baixo da Avanade",
-        "level": 10
+        id: 122,
+        name: "Associate Software Engineer",
+        brc: "A0022",
+        description: "Cargo mais baixo da Avanade",
+        level: 10
     }
 ];
-
