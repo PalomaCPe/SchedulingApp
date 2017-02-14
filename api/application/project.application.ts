fc0756e18fdb7d6aa759666c42c70f03d53f5a18
Arthur <a.hugo.maximiliano@avanade.com>
2017-02-14 10:04:01 -0200

    Project -domain - service, application

--- api/application/project.application.ts
+++ api/application/project.application.ts
@@ -2,7 +2,7 @@ import { Project } from '../domain/project';
 import { ProjectPersistence } from '../persistence/project.persistence';
 
 export class ProjectApplication{
-    getBookings(): Promise<Project[]>{
+    getProjects(): Promise<Project[]>{
         let projectPersistence: ProjectPersistence = new ProjectPersistence();
 
         return projectPersistence.list();
---
+++ api/service/project.service.ts
@@ -0,0 +1,35 @@
+import { Router, Response, Request } from 'express';
+import { Project } from '../domain/project';
+import { ProjectApplication } from '../application/project.application';
+
+export const projectRouter: Router = Router();
+
+projectRouter.get('/list', (request: Request, response: Response) => {
+    let projectApplication: ProjectApplication = new ProjectApplication();
+
+    projectApplication.getProjects()
+        .then((result: Project[]) => {
+            response.json(result);
+        });
+});
+/*
+projectRouter.get('/:id', (request: Request, response: Response) => {
+    let projectApplication: ProjectApplication = new ProjectApplication();
+
+    let id: number = +request.params.id;
+
+    projectApplication.getProject(id).then((result: Project[]) => {
+        response.json(result);
+    });
+});
+
+projectRouter.post('/post', (request: Request, response: Response) => {
+    let projectApplication: ProjectApplication = new ProjectApplication();
+
+    let id: number = +request.params.id;
+
+    projectApplication.getProjects().then((result: Project[]) => {
+        response.json(result);
+    });
+});
+*/
