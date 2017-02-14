fcb3ce045ef60213415cc4044d7dd96151f57b66
Arthur <a.hugo.maximiliano@avanade.com>
2017-02-14 15:16:28 -0200

    project.service

--- app/project/project.service.ts
+++ app/project/project.service.ts
@@ -12,47 +12,32 @@ import { CUSTOMERS } from '../shared/mock';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
 
+    const projectUrl = '/api/project';
+    /*const professionalUrl = '/api/professional';
+    const customerUrl = '/api/customer';*/
+
 @Injectable()
 export class ProjectService {  
-    
-    private projectUrl = '/api/project';
-    private professionalUrl = '/api/professional';
-    private customerUrl = '/api/customer';
 
     constructor(private _http: Http) { }
 
-    /*getListProject(): Promise<Project[]>{
-        return Promise.resolve(PROJECTS);
-    }
-
-    getProjectById(id: number): Promise<Project>{
-        return Promise.resolve(PROJECTS.find(p => p.projectId === id));
-    }
-   
-    getProfessionalList(): Promise<Professional[]>{
-        return Promise.resolve(PROFESSIONALS);
-    } 
-    
-    getCustomers(): Promise<Customer[]>{
-        return Promise.resolve(CUSTOMERS);
-    }     
-    */
     getListProject(): Promise<Project[]>{
-        return this._http.get(this.projectUrl)
+        let url: string = `${projectUrl}/list`;
+        return this._http.get(url)
             .toPromise()
             .then(response => response.json().data as Project[])
             .catch(this.handleError);
     }
 
     getProjectById(id: number): Promise<Project>{
-        const url = `${this.projectUrl}/${id}`;
+        let url: string = `${projectUrl}/${id}`;
         return this._http.get(url)
             .toPromise()
             .then(response => response.json().data as Project)
             .catch(this.handleError);
     }
    
-    getProfessionalList(): Promise<Professional[]>{
+   /* getProfessionalList(): Promise<Professional[]>{
         return this._http.get(this.professionalUrl)
             .toPromise()
             .then(response => response.json().data as Professional[])
@@ -65,7 +50,7 @@ export class ProjectService {
             .then(response => response.json().data as Customer[])
             .catch(this.handleError);
     }
-
+    */
     private handleError(error: any): Promise<any> {
         console.error('An error occurred', error);
         return Promise.reject(error.message || error);
