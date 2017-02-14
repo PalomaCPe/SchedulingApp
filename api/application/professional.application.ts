2c62bb325282cfc4304a9cf621b0d7802b88f1de
Thalita <thalita.silva@avanade.com>
2017-02-14 12:43:23 -0200

    Professional Application Update

--- api/application/professional.application.ts
+++ api/application/professional.application.ts
@@ -2,8 +2,27 @@ import { Professional } from '../domain/professional';
 import { ProfessionalPersistence } from '../persistence/professional.persistence';
 
 export class ProfessionalApplication {
+    constructor(private professionalPersistence: ProfessionalPersistence = new ProfessionalPersistence()) {}
+
     getProfessionals(): Promise<Professional[]> {
-        let professionalPersistence: ProfessionalPersistence = new ProfessionalPersistence();
-        return professionalPersistence.list();
+        return this.professionalPersistence.list();
+    }
+
+    getProfessional(id:number): Promise<Professional> {
+        return this.professionalPersistence.read(id);
+    }
+
+    saveProfessional(professional: Professional): Promise<Professional> {
+        return this.professionalPersistence.update(professional);
+    }
+
+    createProfessional(professional: Professional): Promise<Professional> {
+        professional.deleted = false;
+
+        return this.professionalPersistence.create(professional);
+    }
+
+    deleteProfessional(id: number): Promise<boolean> {
+        return this.professionalPersistence.delete(id);
     }
 }
