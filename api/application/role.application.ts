3c8394aaad9b240e7f64655cb7128dfc29d718ad
mayaracardoso <mayara.lopes.cardoso@avanade.com>
2017-02-14 11:12:27 -0200

    implement role application

--- api/application/role.application.ts
+++ api/application/role.application.ts
@@ -2,9 +2,15 @@ import { Role } from '../domain/role';
 import { RolePersistence } from '../persistence/role.persistence';
 
 export class RoleApplication{
+
     getRoles(): Promise<Role[]>{
         let rolePersistence: RolePersistence = new RolePersistence();
 
         return rolePersistence.list();
+    }
+
+    getRole(id: number): Promise<Role>{
+        let rolePersistence: RolePersistence = new RolePersistence();
+        return rolePersistence.read(id);
     }
 }
