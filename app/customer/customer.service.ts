c77d2b3f4f00c3d17901561a570adad74fa08606
Hebert Paziam <hebert.paziam@avanade.com>
2017-02-14 12:10:49 -0200

    customer.service.ts updated on APP

--- app/customer/customer.service.ts
+++ app/customer/customer.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
 
 import { Customer } from './customer';
 import { CUSTOMERS } from '../shared/mock';
@@ -7,13 +8,22 @@ import { CUSTOMERS } from '../shared/mock';
 @Injectable()
 export class CustomerService {
 
+    constructor(private http: Http) { }
 
     getCustomerById(customer: Customer): Promise<Customer> {
-        return Promise.resolve(CUSTOMERS.find(c => c.id === customer.id))
+        return this.http.get(`/api/customer/${customer.id}`).toPromise()
+            .then((res: Response) => res.json().data || res.json())
+            .catch((error: any) => this.errorHandling(error))
     }
 
     getCustomers(): Promise<Customer[]> {
-        return Promise.resolve(CUSTOMERS);
+        return this.http.get(`/api/customer/list`).toPromise()
+            .then((res: Response) => res.json().data || res.json())
+            .catch((error: any) => this.errorHandling(error))
+    }
+
+    errorHandling(error: any) {
+        console.log(error)
     }
 
 }
