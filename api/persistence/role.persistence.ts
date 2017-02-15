7efa3de76168c33dce85986e892ace49c8c36683
thiagocolen <thiago.souzacolen@gmail.com>
2017-02-15 11:03:11 -0200

    add database on role persistence file

--- api/persistence/role.persistence.ts
+++ api/persistence/role.persistence.ts
@@ -1,3 +1,6 @@
+import { MongoClient, Db, FindAndModifyWriteOpResultObject } from 'mongodb';
+import { Connection } from './connection';
+
 import { Role } from '../domain/role';
 import { ICrud } from './crud.interface';
 
@@ -6,7 +9,20 @@ import { ROLES } from '../../app/shared/mock';
 export class RolePersistence implements ICrud<Role>{
 
     list(): Promise<Role[]>{
-        return Promise.resolve(ROLES);
+        // return Promise.resolve(ROLES);
+        let database: Db;
+        return Promise.resolve(
+            Connection.conn()
+                .then((db: Db) => {
+                    database = db;
+                    return db.collection('role').find({deleted: false}).toArray();
+                })
+                .then((role: Role[]) => {
+                    database.close();
+                    return role;
+                })
+        );
+
     }
 
     read(id: number): Promise<Role>{
